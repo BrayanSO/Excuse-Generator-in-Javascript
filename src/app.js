d57e12b73f6022ced8f790ac0894aadb9391dc87
BrayanSO <briansolera@gmail.com>
2022-11-04 02:20:33 +0000

    java

--- src/app.js
+++ src/app.js
@@ -8,4 +8,28 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let action = ["ate ", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excusa = "";
+
+  excusa = randomVal(who) + "";
+  excusa = excusa + randomVal(action) + " ";
+  excusa = excusa + randomVal(what) + " ";
+  excusa = excusa + randomVal(when);
+
+  document.getElementById("excusa").innerText = excusa;
+  console.log(excusa);
 };
+
+function randomVal(arr) {
+  let num = Math.floor(Math.random() * (arr.length - 1));
+  return arr[num];
+}
